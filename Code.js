7bde02b82a5b4e75b11c27d72af982fe142a5cf7
Thibault Molleman <thibaultmolleman@gmail.com>
2016-09-18 13:53:33 +0200

    Initial Commit

---
+++ Code.js
@@ -0,0 +1,126 @@
+function myFunction() {
+    //show progressbar
+    var html = HtmlService.createTemplateFromFile('loading').evaluate();
+    DocumentApp.getUi().showModalDialog(html, 'Preparing Document...');
+    //df
+    var body = DocumentApp.getActiveDocument().getBody();
+    body.clear();
+    body.setPageWidth(842);
+    body.setPageHeight(595);
+    body.setMarginLeft(28);
+    body.setMarginBottom(28);
+    body.setMarginRight(28);
+    body.setMarginTop(28);
+    var table = body.appendTable();
+    var row = table.appendTableRow().setMinimumHeight(480);
+    row.appendTableCell();
+    row.appendTableCell();
+    table.setColumnWidth(0, 500);
+    var theDocument = DocumentApp.getActiveDocument();
+    var docID = theDocument.getId();
+    var theFile = DriveApp.getFileById(docID);
+    var parents = theFile.getParents();
+    var folder = parents.next();
+    var slidesFolders = folder.getFoldersByName("Slides");
+    var slidesFolder = slidesFolders.next();
+    var files = slidesFolder.getFiles();
+    //files have to be sorted alphabetically for this script to work properly
+    var slides = sortFiles(files);
+    for (var i = 0, len = slides.length; i < len; i++) {
+        var cell = row.getCell(0);
+        cell.appendImage(slides[i]);
+        var progress = i / slides.length * 100;
+        PropertiesService.getScriptProperties().setProperty('convertingProgress', progress);
+        Logger.log(progress);
+        //If there is another slide, prepare cells
+        if (typeof slides[i + 1] !== 'undefined') {
+            var row = table.appendTableRow().setMinimumHeight(480);
+            row.appendTableCell();
+            row.appendTableCell();
+        };
+    }
+    PropertiesService.getScriptProperties().setProperty('convertingProgress', 100);
+}
+
+function cloudConvertTest1() {
+    // Create Process ID
+    var payload = {
+        'inputformat': 'pdf'
+        , 'outputformat': 'jpg'
+    };
+    var headers = {
+        'Authorization': 'Bearer ' + 'CLOUDCONVERT API KEY'
+    };
+    var url = 'https://api.cloudconvert.com/process';
+    var options = {
+        'method': 'post'
+        , 'headers': headers
+        , 'payload': payload
+    };
+    var response = UrlFetchApp.fetch(url, options);
+    var json = response.getContentText();
+    // Receive Process URL
+    var processURL = JSON.parse(json).url;
+    Logger.log('Remaining Minutes (credits)' + JSON.parse(json).minutes);
+    //Starting the conversion
+    var fileToUpload = DriveApp.getFilesByName('testpdfverymuchtest.pdf').next();
+    fileToUpload.setSharing(DriveApp.Access.ANYONE_WITH_LINK, DriveApp.Permission.VIEW);
+    //ZEKER ADVANCED SERVICES INSCHAKELEN VOOR GEAVANCEERDE DRIVE API: https://developers.google.com/apps-script/guides/services/advanced
+    var fileToUploadUrl = fileToUpload.getDownloadUrl();
+    Logger.log(fileToUploadUrl);
+    var payload = {
+        'input': 'download'
+        , 'file': fileToUploadUrl
+        , 'outputformat': 'jpg'
+    };
+    var headers = {
+        'Authorization': 'Bearer ' + 'CLOUDCONVERT API KEY'
+    };
+    var url = 'http:' + processURL;
+    var options = {
+        'method': 'post'
+        , 'headers': headers
+        , 'payload': payload
+    };
+    var response = UrlFetchApp.fetch(url, options);
+    var json = response.getContentText();
+    // Receive Process URL
+    var processURL = JSON.parse(json).url;
+}
+
+function sortFiles(files) {
+    //http://stackoverflow.com/a/36942674
+    // array to hold our data
+    var data = [];
+    // We loop on iterator and append results to an array
+    while (files.hasNext()) {
+        var file = files.next();
+        // we append our data with result, we have now an array of files
+        data.push(file);
+    }
+    // lets sort our array
+    data = data.sort(function (file1, file2) {
+        if (file1.getName().toLowerCase() < file2.getName().toLowerCase()) return -1;
+        else if (file1.getName().toLowerCase() > file2.getName().toLowerCase()) return 1;
+        else return 0;
+    })
+    return data;
+}
+
+function testHTML() {
+    var html = HtmlService.createTemplateFromFile('Loading').evaluate();
+    DocumentApp.getUi().showModalDialog(html, 'Preparing Document...');
+}
+
+function include(filename) {
+    return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+function progress(type) {
+    if (type == "convert") {
+        return PropertiesService.getScriptProperties().getProperty('convertingProgress');
+    }
+    else {
+        return PropertiesService.getScriptProperties().getProperty('AddingProgress');
+    };
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# PDF-to-Google-Docs-Handouts
+This Google Apps Script converts a PDF to handouts in a Google Doc 
+
+
+This project had code from https://github.com/Dogfalo/materialize
+under MIT license: https://github.com/Dogfalo/materialize/blob/master/LICENSE
